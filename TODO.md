9d5e94701b8ec38b43d183126e907b63e97b2ecf
Mateja <stanojevicmateja@gmail.com>
2024-05-10 22:47:26 +0200

    Header and hero sections (WIP, not responsive yet)

---
+++ TODO.md
@@ -0,0 +1,5 @@
+### TODOs for this project:
+
+- [ ] Remove unused font weights
+- [ ] Add meta tags
+- [ ] Add Proxima Nova font
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ['./dist/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'iris-blue': '#002347',
+        'iris-orange': '#FF5C00',
+        'iris-supply-green': '#015C2D',
+        'gray': '#344054',
+        'light-gray': '#D0D5DD'
+      },
+      fontFamily: {
+        'schibsted': ['SchibstedGrotesk', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
