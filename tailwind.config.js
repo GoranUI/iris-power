ca404c23b86a99d27070322426d0c1d78a15bf5c
Mateja <stanojevicmateja@gmail.com>
2024-05-18 03:55:38 +0200

    New design update

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ module.exports = {
         'light-gray': '#D0D5DD',
         'body-gray': '#475467',
         'icon-gray': '#F9F5FF',
+        'lightest-gray': '#F9FAFB',
+        'placeholder-gray': '#667085',
+        'label-gray': '#101828',
       },
       fontFamily: {
         'schibsted': ['Schibsted Grotesk', 'sans-serif'],
@@ -23,6 +26,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
 
